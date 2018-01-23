3529594fd3d6af7cf49393ce427e293b1c8a1854
Chris Marshall <chris@codegoalie.com>
2018-01-23 10:59:35 -0500

    Normalize drip keys

--- README.md
+++ README.md
@@ -16,7 +16,25 @@ Not yet implemented:
 
 ```go
 dripClient = drip.NewClient("DRIP_API_TOKEN", "DRIP_ACCOUNT_ID")
-_ = dripClient.CreateSubscriber("an@email-adress.com", map[string]interface{}{ "Partnerships": "Chris" })
+_ = dripClient.CreateSubscriber("an@email-adress.com", map[string]interface{}{ "first_name": "Chris" })
 _ = dripClient.RecordEvent("an@email-address.com", "Registered")
 _ = dripClient.TagSubscriber("an@email-address.com", "Beta")
 ```
+
+## Features
+
+### Key normailzation
+
+`go-drip` will normalize custom fields keys by removing `$` characters,
+replacing spaces with underscores, and lower casing the entire key. For
+example:
+
+```
+"First Name" => "first_name"
+"$email" => "email"
+```
+
+Drip will not add any custom fields even if only one key is malformed. This
+feature tries to guard you against that and helps avoid users (you)
+implementing the same transformations. I'm happy to accept or add additional
+transformations, if you'd benefit from them.
--- drip.go
+++ drip.go
@@ -1,15 +1,21 @@
 package drip
 
 import (
-	"github.com/parnurzeal/gorequest"
+	"bytes"
+	"encoding/json"
+	"net"
+	"net/http"
+	"strings"
+	"time"
 )
 
 const baseURL = "https://api.getdrip.com/v2/"
 
-// Client can interact with Drip via their REST API
+// Client represents a Drip API client wrapper
 type Client struct {
-	request *gorequest.SuperAgent
-	appID   string
+	apiKey     string
+	appID      string
+	httpClient http.Client
 }
 
 type subParams struct {
@@ -41,41 +47,50 @@ type tagParams struct {
 
 // NewClient returns a client instance ready to act with Drip for the given app and API key
 func NewClient(apiKey, appID string) *Client {
-	return &Client{
-		request: gorequest.New().SetBasicAuth(apiKey, ""),
-		appID:   appID,
+	client := &Client{
+		apiKey: apiKey,
+		appID:  appID,
+		httpClient: http.Client{
+			Timeout: time.Second * 10,
+			Transport: &http.Transport{
+				Dial: (&net.Dialer{
+					Timeout: time.Second * 5,
+				}).Dial,
+				TLSHandshakeTimeout: time.Second * 5,
+			},
+		},
 	}
+
+	return client
 }
 
 // CreateSubscriber creates a new or updates an existing subscriber by email
 func (c Client) CreateSubscriber(email string, customFields map[string]interface{}) error {
-	data := subRoot{
+	dripFields := map[string]interface{}{}
+	for key, value := range customFields {
+		newKey := key
+		newKey = strings.Replace(key, "$", "", -1)
+		newKey = strings.Replace(newKey, " ", "_", -1)
+		newKey = strings.ToLower(newKey)
+		dripFields[newKey] = value
+	}
+	bodyData := subRoot{
 		Subscribers: []subParams{
-			{Email: email, CustomFields: customFields},
+			{Email: email, CustomFields: dripFields},
 		},
 	}
-	_, _, errs := c.request.Post(baseURL + c.appID + "/subscribers").Send(data).End()
-	if errs != nil {
-		return errs[0]
-	}
 
-	return nil
-
+	return c.authenticatedPost("/subscribers", bodyData)
 }
 
 // RecordEvent sends a custom event to Drip
 func (c Client) RecordEvent(email, eventName string) error {
-	data := eventRoot{
+	bodyData := eventRoot{
 		Events: []eventParams{
 			{Email: email, Action: eventName},
 		},
 	}
-	_, _, errs := c.request.Post(baseURL + c.appID + "/events").Send(data).End()
-	if errs != nil {
-		return errs[0]
-	}
-
-	return nil
+	return c.authenticatedPost("/events", bodyData)
 }
 
 // TagSubscriber adds a tag to a subscriber
@@ -85,11 +100,18 @@ func (c Client) TagSubscriber(email, tag string) error {
 			{Email: email, Tag: tag},
 		},
 	}
+	return c.authenticatedPost("/tags", data)
+}
 
-	_, _, errs := c.request.Post(baseURL + c.appID + "/tags").Send(data).End()
-	if errs != nil {
-		return errs[0]
+func (c Client) authenticatedPost(path string, body interface{}) error {
+	postBody, err := json.Marshal(body)
+	if err != nil {
+		return err
 	}
-
-	return nil
+	req, err := http.NewRequest("POST", baseURL+c.appID+path, bytes.NewReader(postBody))
+	req.SetBasicAuth(c.apiKey, "")
+	req.Header.Add("Accept", "application/vnd.api+json")
+	req.Header.Add("Content-Type", "application/json")
+	_, err = c.httpClient.Do(req)
+	return err
 }
